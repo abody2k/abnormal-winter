0c8870164532a593e08dbe961068dcefa60078db
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-01-31 21:35:39 +0300

    removed plant after crops finished, planted a plant, animated it as well

--- scripts/land.gd
+++ scripts/land.gd
@@ -4,7 +4,7 @@ extends Node3D
 enum STATES {IDLE, GROWING}
 
 var state : STATES = STATES.IDLE
-
+const PLANT = preload("res://scenes/plant.tscn")
 func is_idle():
 	if state == STATES.IDLE:
 		return true
@@ -14,7 +14,15 @@ func is_idle():
 
 func grow():
 	state = STATES.GROWING
+	var plant = PLANT.instantiate()
+	add_child(plant)
+	plant.position = Vector3(0,0.97,0)
+	plant.finished_growing.connect(finished)
 	
+	
+func finished():
+	get_tree().call_group("machine","update_land",self)
+
 	
 func _ready():
 	get_tree().call_group("machine","add_land",self)
--- scripts/plant.gd
+++ scripts/plant.gd
@@ -4,6 +4,14 @@ extends Node3D
 
 signal finished_growing
 
-
+func _ready():
+	$AnimationPlayer.play("plant_growing")
+	
+	
 func _on_animation_player_animation_finished(anim_name):
-	pass # Replace with function body.
+	if anim_name == "plant_growing":
+		$AnimationPlayer.play("tree_blowing")
+	else:
+		finished_growing.emit()
+		queue_free()
+	

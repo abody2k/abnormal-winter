a01487ba641279b2ba34f0f99ff69ee092215cdb
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-01-31 21:17:54 +0300

    increased number of bags in the machine, and added a function to check if a land is actually free or not

--- scripts/land.gd
+++ scripts/land.gd
@@ -1,6 +1,16 @@
 extends Node3D
 
 
+enum STATES {IDLE, GROWING}
+
+var state : STATES = STATES.IDLE
+
+func is_idle():
+	if state == STATES.IDLE:
+		return true
+	else:
+		return false
+
 func _ready():
 	get_tree().call_group("machine","add_land",self)
 	pass 
--- scripts/machine.gd
+++ scripts/machine.gd
@@ -4,6 +4,8 @@ extends MeshInstance3D
 
 var lands = []
 
+var no_of_bags = 0
+
 
 func add_land(land):
 	lands.append(land)
@@ -22,4 +24,6 @@ func _process(delta):
 func _on_area_3d_body_entered(body):
 	if body.has_bag:
 		body.remove_bag()
+		no_of_bags+=1
+		
 		

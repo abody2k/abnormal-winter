445d114d956422647704b20493eddc1a76fd600d
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-01-31 19:42:17 +0300

    fixed bullet orientation

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,12 +1,8 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
-
 func _physics_process(delta):
-	velocity = basis.z * -200 
+	velocity = basis.x * 2
 	var target = move_and_collide(velocity)
 	if target:
 		target.get_collider().queue_free()

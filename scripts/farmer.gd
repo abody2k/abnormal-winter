8b81e0ebaa1814b165acf0235d3e3553460deace
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-01-31 18:56:50 +0300

    farmer rotation

--- scripts/farmer.gd
+++ scripts/farmer.gd
@@ -9,14 +9,19 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+		move_and_slide()
+		return
+		
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("fire"):
+		#fire a bullet
+		pass
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	$farmer_rigged.rotate_y(input_dir.x * delta)
+	return
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED

13c97c29e4379811bbe2c3c64696de7ac1f09696
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-01-31 21:57:56 +0300

    added state machine to the chicken

--- scripts/chicken.gd
+++ scripts/chicken.gd
@@ -2,6 +2,12 @@ extends CharacterBody3D
 
 
 
+enum MODES {IDLE, ATTACKING}
+
+
+var mode : MODES = MODES.IDLE
+
+
 var enemies = []
 
 
@@ -10,32 +16,23 @@ var target : CharacterBody3D = null
 
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
+@export var SPEED = 20.0
+@export var distance = 10.0
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
-
+	match mode:
+		MODES.IDLE:
+			if target:
+				if position.distance_to(target.position) > distance:
+					look_at(target.position)
+					velocity = -basis.z * SPEED
+					move_and_slide()
+					$AnimationPlayer.play("chicken_walk")
+				else:
+					$AnimationPlayer.play("chicken_idle")
+					mode = MODES.ATTACKING
+		MODES.ATTACKING:
+			$AnimationPlayer.play("chicken_attack")
 
 
 func _on_detector_body_entered(body):
@@ -61,3 +58,8 @@ func remove_enemy(enemy):
 
 func _on_detector_body_exited(body):
 	remove_enemy(body)
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "chicken_attack":
+		pass

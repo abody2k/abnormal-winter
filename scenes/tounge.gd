6c512f232a595d61eb8c10422d240a2ca630b8a8
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-02-01 11:11:32 +0300

    fixed tounge monster faulty coordinates

--- scenes/tounge.gd
+++ scenes/tounge.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 
-const SPEED = 20.0
+const SPEED = 10.0
 const JUMP_VELOCITY = 4.5
 signal death
 
@@ -15,21 +15,27 @@ var index = 0
 
 func _ready():
 	print((get_tree().get_first_node_in_group("path") as Path3D).curve.get_point_position(index))
-	current_target = (get_tree().get_first_node_in_group("path") as Path3D).curve.get_point_in(index)
-	current_target.y = position.y
+	current_target =(get_tree().get_first_node_in_group("path") as Path3D).position + (get_tree().get_first_node_in_group("path") as Path3D).curve.get_point_position(index)
+
 
 func _physics_process(delta):
 	
 	match mode:
 		MODES.WALKING:
 			if position.distance_to(Vector3(current_target.x,position.y,current_target.z) ) > 10.0:
+				
 				look_at(Vector3(current_target.x,position.y,current_target.z))
-				velocity =( basis.z + Vector3.DOWN * 10) * SPEED 
+				velocity =( -basis.z + Vector3.DOWN * 10) * SPEED 
 				move_and_slide()
+				print("reaching...")
+				print(position)
+				print(current_target)
+				
+				print((global_position - (Vector3(current_target.x,position.y,current_target.z) )).length())
 			else:
+				print("reached goal")
 				index +=1
-				current_target = (get_tree().get_first_node_in_group("path") as Path3D).curve.get_point_in(index)
-				current_target.y = position.y
+				current_target =(get_tree().get_first_node_in_group("path") as Path3D).position + (get_tree().get_first_node_in_group("path") as Path3D).curve.get_point_position(index)
 
 func _exit_tree():
 	death.emit(self)

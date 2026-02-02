19201245c153876a4dcc8c2cf990b30db3647a00
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-02-02 09:45:01 +0300

    limiting the number of worm and tounge monsters

--- scenes/main.gd
+++ scenes/main.gd
@@ -4,7 +4,16 @@ extends Node3D
 const WORM = preload("res://scenes/worm.tscn")
 const TOUNGE = preload("res://scenes/tounge.tscn")
 const FOOD = preload("res://scenes/bag.tscn")
-# Called when the node enters the scene tree for the first time.
+
+
+var global_worms = 0 
+var global_tounge = 0
+
+const MAXIMUM_NO_OF_WORMS = 30
+const MAXIMUM_NO_OF_TOUNGES = 30
+
+
+
 func _ready():
 	pass # Replace with function body.
 
@@ -15,14 +24,22 @@ func _process(delta):
 
 
 func _on_timer_timeout():
-	var worm = WORM.instantiate()
-	add_child(worm)
-	worm.position = $loc.position
-	var tounge = TOUNGE.instantiate()
-	add_child(tounge)
-	tounge.position = $loc.position
-	pass # Replace with function body.
+	if global_worms < MAXIMUM_NO_OF_WORMS :
+		var worm = WORM.instantiate()
+		add_child(worm)
+		worm.position = $loc.position
+		global_worms+=1
+	if global_tounge < MAXIMUM_NO_OF_TOUNGES :
+		var tounge = TOUNGE.instantiate()
+		add_child(tounge)
+		tounge.position = $loc.position
+		global_tounge+=1
 
+func worm_died():
+	global_worms-=1
+	
+func tounge_died():
+	global_tounge-=1
 
 func _on_materials_timeout():
 	var bag = FOOD.instantiate()
